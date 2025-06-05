10d28793b2209af893fb6dc14787ee04b78297ba
hyser521 <eric.m.lew@gmail.com>
2025-06-05 11:23:53 -0500

    create Player class; create detection circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     #Init import(s)
@@ -7,8 +8,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
