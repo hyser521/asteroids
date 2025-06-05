c538d0dfa864a1e18760008ea3cf1059de7a7357
hyser521 <eric.m.lew@gmail.com>
2025-06-05 11:05:03 -0500

    add game clock

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from constants import *
 def main():
     #Init import(s)
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        pygame.Surface.fill(screen, (255,255,255))
+        pygame.Surface.fill(screen, (0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             pygame.display.flip()
+            dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
